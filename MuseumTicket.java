ac0f39d249f713b3f13603bb8d0573a9891b7326
EddieResendizRRHS <73211323+EddieResendizRRHS@users.noreply.github.com>
2020-11-13 13:29:12 -0600

    Create MuseumTicket.java

---
+++ MuseumTicket.java
@@ -0,0 +1,59 @@
+
+/**
+ * Write a description of class MuseumTicket here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MuseumTicket
+{
+    // instance variables - replace the example below with your own
+    private String Day;
+    private int age ;
+    private double price;
+    private String coupon;
+    
+    /**
+     * Constructor for objects of class MuseumTicket
+     */
+    
+    // Author: 
+    public MuseumTicket()
+    {
+        // initialise instance variables
+        
+    }
+    
+    // Author: 
+    public MuseumTicket( String myDay, int myAge, String myCoupon)
+    {
+        
+        
+    }
+    
+    // Author: 
+    public void setTickets ( String myDay, int myAge, String myCoupon)
+    {
+        
+        
+    }
+   
+    // Author: 
+    public void calculatePrice()
+    {
+      // if statements to decide assign price to the ticket
+        
+    }
+    
+    // Author: 
+    public double getPrice()
+    {
+        return price;
+    }
+    
+    // Author: 
+    public void printTicket()
+    {
+        // print the ticket  
+    }
+}
