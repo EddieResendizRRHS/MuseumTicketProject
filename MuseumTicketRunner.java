ed61c7798ab4488583b71d9289e90b3de9159d3f
EddieResendizRRHS <73211323+EddieResendizRRHS@users.noreply.github.com>
2020-11-13 13:31:10 -0600

    Create MuseumTicketRunner.java

---
+++ MuseumTicketRunner.java
@@ -0,0 +1,45 @@
+/**
+ * Write a description of class MuseumTicketRunner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+// 1.  import Scanner 
+// Completed by: 
+
+public class MuseumTicketRunner
+{
+    public static void main(String[] args)
+    {
+        // instantiate an object of Class Scanner // Completed by: 
+        
+        // create a variable of totalPrice // Completed by: 
+        
+        // instantiate ticket Object using default constructor // Completed by: 
+        
+        // ask the user for how many tickets // Completed by: 
+        
+        // save the value in a variable numTickets of type int // Completed by: 
+        
+        // write a for loop // Completed by: 
+        
+        
+            // ask the user to input  day , age and discount coupon // Completed by: 
+            
+            
+            // call the method setTicket() with arguments day, age , discount // Completed by: 
+            
+            
+            // call method printTicket() // Completed by: 
+            
+            
+            // call method calculatePrice() // Completed by:  
+           
+            // call getPrice() and add the value to totalPrice // Completed by: 
+           
+         
+         // print total ticket price // Completed by: 
+            
+        
+    }
+}
